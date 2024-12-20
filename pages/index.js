146e2186d3bf0edc852c041ea3021106c011784d
oscarczernuszyn <u7282674@anu.edu.au>
2024-12-20 23:38:20 +1100

    integrate chakra styling properly

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
-const Page = ( ) => {
-    return <div>Hello</div>
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Welcome to my domain
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Oscar Czernuszyn
+          </Heading>
+          <p>Full stack software engineer. Aspiring game developer</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
