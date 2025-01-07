8eaab678d499cceae98563323958b978a1d3e636
oscarczernuszyn <u7282674@anu.edu.au>
2025-01-07 22:28:46 +1100

    rework and populate experience bar

--- components/experience.js
+++ components/experience.js
@@ -13,50 +13,75 @@ const ExperienceBar = () => {
     iconBackground: useColorModeValue('whiteAlpha.900', 'gray.800')
   }
 
+  const elementStyles = {
+    contentStyle: {
+      background: timelineColors.background,
+      color: timelineColors.text
+    },
+    contentArrowStyle: {
+      borderRight: `7px solid ${timelineColors.background}`
+    },
+    iconStyle: {
+      background: timelineColors.iconBackground,
+      color: timelineColors.text,
+      width: '48px',
+      height: '48px'
+    }
+  }
+
+  const renderElement = (date, iconSrc, title, subtitle, description) => (
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      date={date}
+      {...elementStyles}
+      icon={
+        <Image
+          borderRadius="full"
+          background={timelineColors.iconBackground}
+          src={iconSrc}
+          alt="icon"
+          padding="4px"
+        />
+      }
+    >
+      <Heading as="h3" size="md" mb={2}>
+        {title}
+      </Heading>
+      <Heading as="h4" size="sm" mb={2}>
+        {subtitle}
+      </Heading>
+      <Text>{description}</Text>
+    </VerticalTimelineElement>
+  )
+
   return (
     <Box>
       <VerticalTimeline
         lineColor={timelineColors.background}
-        layout={'1-column-left'}
-        animate={true}
+        layout="1-column"
+        animate
       >
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: timelineColors.background,
-            color: timelineColors.text
-          }}
-          contentArrowStyle={{
-            borderRight: `7px solid ${timelineColors.background}`
-          }}
-          date="May 2024 - August 2024"
-          iconStyle={{
-            background: timelineColors.iconBackground,
-            color: timelineColors.text,
-            width: '48px',
-            height: '48px'
-          }}
-          icon={
-            <Box>
-              <Image
-                padding="1px"
-                paddingLeft="1px"
-                background="white"
-                borderRadius="50%"
-                src={'/images/bleh.png'}
-              />
-            </Box>
-          }
-        >
-          <Heading as="h3" size="md" mb={2}>
-            penten
-          </Heading>
-          <Heading as={'h4'} size={'sm'} mb={2}>
-            software engineer intern
-          </Heading>
-          <Text>responsible for</Text>
-          <Text>filler</Text>
-        </VerticalTimelineElement>
+        {renderElement(
+          'Dec 2023 - Present',
+          '/images/penten.png',
+          'penten',
+          'software engineer intern',
+          "while originally hired to complete a 6 month project on improving offline model generation, i now work as a fullstack engineer on two of the company's flagship projects; HoneyTrace and the Content Behaviour Generator"
+        )}
+        {renderElement(
+          'Feb 2023 - Present',
+          '/images/anu.png',
+          'australian national university',
+          'casual seasonal academic',
+          "for the last two years i've worked with course convenors and other teaching staff to run classes, mark exams and support students. in that time ive taken courses in strucuted programming (in java), formal logic, and artifical intelligence"
+        )}
+        {renderElement(
+          'Apr 2023 - August 2023',
+          '/images/canteen.png',
+          'canteen',
+          'data analysis intern',
+          "in the few months i volunteered as a data analyst for the charity canteen i worked on developing a SARIMA model that could be used to predict the organisation's yearly income from regular donors"
+        )}
       </VerticalTimeline>
     </Box>
   )
